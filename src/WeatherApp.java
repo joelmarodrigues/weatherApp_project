a0399d6a2ae4f4720430fd4704a2bfeed112f640
Joelma Rodrigues <113507468+joelmarodrigues@users.noreply.github.com>
2023-05-02 18:32:57 +0100

    Update WeatherApp.java

--- src/WeatherApp.java
+++ src/WeatherApp.java
@@ -1,23 +1,51 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class WeatherApp {
-    private WeatherAPI weatherAPI;
-    private WeatherGUI weatherGUI;
-    
-    public WeatherApp(WeatherAPI weatherAPI) {
-        this.weatherAPI = weatherAPI;
-        this.weatherGUI = new WeatherGUI(this);
+    private final String apiKey;
+
+    public WeatherApp(String apiKey) {
+        this.apiKey = apiKey;
     }
-    
+
     public String getWeatherData(String location) throws IOException {
-        String weatherData = weatherAPI.getWeatherData(location);
-        weatherGUI.displayWeatherData(weatherData);
-        return weatherData;
+        // Encode the location string for use in the URL
+        String encodedLocation = URLEncoder.encode(location, StandardCharsets.UTF_8);
+
+        // Construct the URL of the weather API endpoint
+        String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + encodedLocation + "&appid=" + apiKey;
+        URI uri = URI.create(apiUrl);
+        URL url = uri.toURL();
+
+        // Open a connection to the API endpoint and get the response as a string
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        InputStream responseStream = connection.getInputStream();
+        Scanner scanner = new Scanner(responseStream, StandardCharsets.UTF_8);
+        String responseBody = scanner.useDelimiter("\\A").next();
+
+        // Close the resources
+        scanner.close();
+        responseStream.close();
+        connection.disconnect();
+
+        return responseBody;
     }
-    
+
     public static void main(String[] args) {
-        String apiKey = "2e36f31b1146ad8813f4a891c96327bc";
-        WeatherAPI weatherAPI = new WeatherAPI(apiKey);
-        new WeatherApp(weatherAPI);
+        String apiKey = "YOUR_API_KEY_HERE";
+        WeatherApp weatherApp = new WeatherApp(apiKey);
+
+        try {
+            String weatherData = weatherApp.getWeatherData("Toronto");
+            System.out.println(weatherData);
+        } catch (IOException e) {
+            System.out.println("Error retrieving weather data.");
+        }
     }
 }
