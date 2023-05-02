046541be1584404246a1c4fb4dbdb7d162c8d654
Joelma Rodrigues <113507468+joelmarodrigues@users.noreply.github.com>
2023-05-03 00:17:38 +0100

    bottomPanel

--- src/WeatherGUI.java
+++ src/WeatherGUI.java
@@ -9,14 +9,17 @@ import org.json.JSONObject;
 public class WeatherGUI extends JFrame {
     private JTextField searchField;
     private JLabel dateTimeLabel, locationLabel, temperatureLabel, summaryLabel, timezoneLabel;
-    private JTextArea weatherTextArea;
+    private JLabel visibilityLabel;
+    private JLabel humidityLabel;
+    private JLabel windSpeedLabel;
+    private JLabel feelsLikeLabel;
     private JButton searchButton;
 
     public WeatherGUI(WeatherApp weatherApp) {
         super("Weather App");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResizable(false);
-        setPreferredSize(new Dimension(600, 800));
+        // setResizable(false);
+        setPreferredSize(new Dimension(600, 600));
         setLayout(new FlowLayout()); // Set the layout to FlowLayout
 
         // Create the search field with suggestion
@@ -27,6 +30,8 @@ public class WeatherGUI extends JFrame {
                 if (searchField.getText().equals("Search city")) {
                     searchField.setText("");
                 }
+                // Reset search field border color
+                searchField.setBorder(BorderFactory.createLineBorder(Color.BLACK));
             }
 
             public void focusLost(FocusEvent e) {
@@ -36,7 +41,7 @@ public class WeatherGUI extends JFrame {
             }
         });
 
-        // Create a key listener for the search field
+        // Create a key (Enter) listener for the search field
         searchField.addKeyListener(new KeyAdapter() {
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
@@ -44,8 +49,11 @@ public class WeatherGUI extends JFrame {
                         String location = searchField.getText();
                         String weatherData = weatherApp.getWeatherData(location);
                         displayWeatherData(weatherData);
+                        searchField.setBorder(BorderFactory.createLineBorder(Color.GRAY)); // reset border color
+
                     } catch (IOException ex) {
-                        weatherTextArea.setText("Error retrieving weather data.");
+                        searchField.setBorder(BorderFactory.createLineBorder(Color.RED));
+                        JOptionPane.showMessageDialog(WeatherGUI.this, "Error retrieving weather data.");
                     }
                 }
             }
@@ -61,12 +69,16 @@ public class WeatherGUI extends JFrame {
                     String location = searchField.getText();
                     String weatherData = weatherApp.getWeatherData(location);
                     displayWeatherData(weatherData);
+                    searchField.setBorder(BorderFactory.createLineBorder(Color.GRAY)); // reset border color
                 } catch (IOException ex) {
-                    weatherTextArea.setText("Error retrieving weather data.");
+                    searchField.setBorder(BorderFactory.createLineBorder(Color.RED));
+                    JOptionPane.showMessageDialog(WeatherGUI.this, "Error retrieving weather data.");
                 }
             }
         });
+        
 
+        // TOP PANEL
         // Create the search bar panel with search field and button
         JPanel searchPanel = new JPanel();
         searchPanel.setLayout(new FlowLayout());
@@ -80,12 +92,12 @@ public class WeatherGUI extends JFrame {
         topPanel.setPreferredSize(new Dimension(600, 50));
         topPanel.add(searchPanel, BorderLayout.NORTH);
 
+        // MIDDLE PANEL
         // Create the middle panel with location and temperature labels
         JPanel middlePanel = new JPanel();
         middlePanel.setLayout(new BoxLayout(middlePanel, BoxLayout.Y_AXIS));
         middlePanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
         middlePanel.setPreferredSize(new Dimension(600, 300));
-
 
         // Create the location label
         JLabel locationLabel = getLocationLabel();
@@ -117,18 +129,41 @@ public class WeatherGUI extends JFrame {
         dateTimeLabel.setAlignmentY(Component.CENTER_ALIGNMENT);
         middlePanel.add(dateTimeLabel);
 
-        // Create the bottom panel with weather data
-        weatherTextArea = new JTextArea();
-        weatherTextArea.setEditable(false);
-        weatherTextArea.setLineWrap(true);
-        weatherTextArea.setWrapStyleWord(true);
-        weatherTextArea.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        weatherTextArea.setBackground(getBackground());
+        // BOTTOM PANEL
+        // Create the bottom panel with weather data labels
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.Y_AXIS));
+        bottomPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
+        bottomPanel.setPreferredSize(new Dimension(600, 200));
+
+        // Create the feels-like temperature label
+        JLabel feelsLikeLabel = getFeelsLikeLabel();
+        feelsLikeLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        feelsLikeLabel.setAlignmentY(Component.CENTER_ALIGNMENT);
+        bottomPanel.add(feelsLikeLabel);
+
+        // Create the visibility label
+        JLabel visibilityLabel = getVisibilityLabel();
+        visibilityLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        visibilityLabel.setAlignmentY(Component.CENTER_ALIGNMENT);
+        bottomPanel.add(visibilityLabel);
+
+        // Create the humidity label
+        JLabel humidityLabel = getHumidityLabel();
+        humidityLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        humidityLabel.setAlignmentY(Component.CENTER_ALIGNMENT);
+        bottomPanel.add(humidityLabel);
+
+        // Create the wind speed label
+        JLabel windSpeedLabel = getWindSpeedLabel();
+        windSpeedLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        windSpeedLabel.setAlignmentY(Component.CENTER_ALIGNMENT);
+        bottomPanel.add(windSpeedLabel);
 
         // Add the panels to the frame
         add(topPanel, BorderLayout.NORTH);
         add(middlePanel, BorderLayout.CENTER);
-        add(weatherTextArea, BorderLayout.SOUTH);
+        add(bottomPanel, BorderLayout.SOUTH);
 
         // Set the size of the frame and make it visible
         pack();
@@ -139,7 +174,7 @@ public class WeatherGUI extends JFrame {
     public void displayWeatherData(String weatherData) {
         // Parse the weather data and update the labels
         JSONObject json = new JSONObject(weatherData);
-    
+
         // Format the date and time strings
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy");
@@ -147,45 +182,41 @@ public class WeatherGUI extends JFrame {
         String date = now.format(dateFormatter);
         String time = now.format(timeFormatter);
 
+        // Update the date/time and location labels
         dateTimeLabel.setText(date + " | " + time);
-
         String locationName = json.getString("name");
         String locationCountry = json.getJSONObject("sys").getString("country");
         locationLabel.setText(locationName + ", " + locationCountry);
-    
-        JSONObject main = json.getJSONObject("main");
-        int temperature = (int) (main.getDouble("temp") - 273.15); // Convert from Kelvin to Celsius
-        int feelsLike = (int) (main.getDouble("feels_like") - 273.15); // Convert from Kelvin to Celsius
-    
-        JSONObject weather = json.getJSONArray("weather").getJSONObject(0);
-        String summary = weather.getString("description");
-        summary = summary.substring(0, 1).toUpperCase() + summary.substring(1); // Capitalize the first letter
-    
-        String formattedSummary = String.format("%s. Feels like %d°C.", summary, feelsLike);
-    
-        int visibility = json.getInt("visibility") / 1000; // Convert from meters to kilometers
-        int humidity = main.getInt("humidity");
-        double windSpeed = json.getJSONObject("wind").getDouble("speed");
 
-        String timezone = json.optString("timezone", ""); // Use optString() instead of getString()
-        timezoneLabel.setText("Timezone: " + timezone);
-        
+        // Update the timezone label
         int timezoneOffsetInSeconds = json.getInt("timezone");
         int timezoneOffsetInHours = timezoneOffsetInSeconds / 3600; // Convert seconds to hours
         String timezoneOffsetStr = String.format("%+03d", timezoneOffsetInHours); // Format the offset as "+hh" or "-hh"
-        
-        timezoneLabel.setText("Timezone: UTC " + timezoneOffsetStr +  ":00"); // Display the timezone in the format "UTC±hh:00"        
+        String timezoneLabelText = "Timezone: UTC " + timezoneOffsetStr + ":00"; // Display the timezone in the format
+                                                                                 // "UTC±hh:00"
+        timezoneLabel.setText(timezoneLabelText);
 
-        String weatherInfo = "Visibility: " + visibility + " km\n"
-                + "Humidity: " + humidity + "%\n"
-                + "Wind Speed: " + windSpeed + " m/s\n";
-    
+        // Update the middle panel labels
+        JSONObject main = json.getJSONObject("main");
+        int temperature = (int) (main.getDouble("temp") - 273.15); // Convert from Kelvin to Celsius
+        int feelsLike = (int) (main.getDouble("feels_like") - 273.15); // Convert from Kelvin to Celsius
+        JSONObject weather = json.getJSONArray("weather").getJSONObject(0);
+        String summary = weather.getString("description");
+        summary = summary.substring(0, 1).toUpperCase() + summary.substring(1); // Capitalize the first letter
         summaryLabel.setText(summary);
         temperatureLabel.setText(temperature + "°C");
-        weatherTextArea.setText(formattedSummary + "\n\n" + weatherInfo);
+        feelsLikeLabel.setText("Feels like " + feelsLike + "°C");
+
+        // Update the bottom panel labels
+        int visibility = json.getInt("visibility") / 1000; // Convert from meters to kilometers
+        int humidity = main.getInt("humidity");
+        double windSpeed = json.getJSONObject("wind").getDouble("speed");
+        visibilityLabel.setText("Visibility: " + visibility + " km");
+        humidityLabel.setText("Humidity: " + humidity + "%");
+        windSpeedLabel.setText("Wind Speed: " + windSpeed + " m/s");
     }
 
-    //middlePanel JLabels Start Here
+    // middlePanel JLabels Start Here
 
     private JLabel getLocationLabel() {
         locationLabel = new JLabel();
@@ -196,7 +227,7 @@ public class WeatherGUI extends JFrame {
 
     private JLabel getDateTimeLabel() {
         dateTimeLabel = new JLabel();
-        dateTimeLabel.setFont(new Font("Helvetica", Font.PLAIN, 14));
+        dateTimeLabel.setFont(new Font("Helvetica", Font.PLAIN, 16));
         dateTimeLabel.setHorizontalAlignment(JLabel.CENTER);
         return dateTimeLabel;
     }
@@ -217,12 +248,35 @@ public class WeatherGUI extends JFrame {
 
     private JLabel getTimezoneLabel() {
         timezoneLabel = new JLabel();
-        timezoneLabel.setFont(new Font("Helvetica", Font.PLAIN, 14));
+        timezoneLabel.setFont(new Font("Helvetica", Font.PLAIN, 16));
         timezoneLabel.setHorizontalAlignment(JLabel.CENTER);
-        
         return timezoneLabel;
     }
 
-    //middlePanel Label finishes here
+    // middlePanel Label finishes here
+
+    private JLabel getFeelsLikeLabel() {
+        feelsLikeLabel = new JLabel();
+        feelsLikeLabel.setFont(new Font("Helvetica", Font.PLAIN, 18));
+        return feelsLikeLabel;
+    }
+
+    private JLabel getVisibilityLabel() {
+        visibilityLabel = new JLabel();
+        visibilityLabel.setFont(new Font("Helvetica", Font.PLAIN, 18));
+        return visibilityLabel;
+    }
+
+    private JLabel getHumidityLabel() {
+        humidityLabel = new JLabel();
+        humidityLabel.setFont(new Font("Helvetica", Font.PLAIN, 18));
+        return humidityLabel;
+    }
+
+    private JLabel getWindSpeedLabel() {
+        windSpeedLabel = new JLabel();
+        windSpeedLabel.setFont(new Font("Helvetica", Font.PLAIN, 18));
+        return windSpeedLabel;
+    }
 
 }
